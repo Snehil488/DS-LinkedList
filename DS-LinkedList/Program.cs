667fc753b85866cb2da7adffbc3dfa09f8305e1b
Snehil <snehilverma488@gmail.com>
2020-10-21 19:52:49 +0530

    [Snehil] Add : Created Simple LinkedList

---
+++ DS-LinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DS_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(70);
+            linkedList.Add(30);
+            linkedList.Add(56);
+            linkedList.Display();
+        }
+    }
+}
